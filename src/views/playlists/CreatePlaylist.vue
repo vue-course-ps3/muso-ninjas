40d6991688ba087aec2bdbc58d0eefcea03fb031
maxim-peeters <maximpeeters38@gmail.com>
2025-01-08 22:42:59 +0100

    lesson 148 - storage doesnt work on free tier anymore

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -5,9 +5,9 @@
       <input type="text" required placeholder="Playlist title" v-model="title">
       <textarea required placeholder="Playlist description..." v-model="description"></textarea>
       <!-- upload playlist image -->
-      <label>Upload Playlist Cover Image</label>
-      <input type="file">
-      <div class="error"></div>
+      <!-- <label>Upload Playlist Cover Image</label>
+      <input type="file" @change="handleChange">
+      <div class="error">{{ fileError }}</div> -->
 
       <button>Create</button>
     </form>
@@ -21,12 +21,32 @@ export default {
   setup() {
     const title = ref('')
     const description = ref('')
+    const file = ref(null)
+    const fileError = ref(null)
 
     const handleSubmit = () => {
-     console.log(title.value, description.value)
+      if (file.value) {
+        console.log(title.value, description.value, file.value)
+      }
     }
+
+    // allowed file types
+    // const types = ['image/png', 'image/jpeg']
+
+    // const handleChange = (e) => {
+    //   let selected = e.target.files[0]
+    //   console.log(selected)
+
+    //   if (selected && types.includes(selected.type)) {
+    //     file.value = selected
+    //     fileError.value = null
+    //   } else {
+    //     file.value = null
+    //     fileError.value = 'Please select an image file (png or jpg)'
+    //   }
+    //}
     
-    return { title, description, handleSubmit }
+    return { title, description, handleSubmit, fileError }
   }
 }
 </script>
