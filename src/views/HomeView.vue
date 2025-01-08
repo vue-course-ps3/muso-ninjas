8a219604183624f8097ee27f8626256ab4129bf6
maxim-peeters <maximpeeters38@gmail.com>
2025-01-08 22:53:55 +0100

    lesson 150

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="home">
-   <p>Homepage</p>
+   <div class="home">
+    <div v-if="error" class="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection';
+
 export default {
   name: 'HomeView',
-  
+  setup(){
+    const {error, documents} = getCollection('playlists');
+
+    return {error, documents}
+  }
 }
 </script>
