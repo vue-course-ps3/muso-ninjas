1e448d0572bacb1dd93c6fbc1101b622a2953b32
maxim-peeters <maximpeeters38@gmail.com>
2025-01-08 22:14:39 +0100

    lesson 139

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '@/views/auth/Login.vue'
 
 const routes = [
   {
@@ -7,6 +8,12 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+
  
 ]
 
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" required>
+    <input type="password" placeholder="Password" v-model="password" required>
+    <div class="error" v-if="error">{{ error }}</div>
+    <button v-if="!isPending">Log in</button>
+    <button v-if="isPending" disabled>Loading</button>
+
+  </form>
+</template>
+
+<script>
+import useLogin from '@/composables/useLogin';
+import { ref } from 'vue';
+
+export default {
+  setup(){
+    const {error, login, isPending} = useLogin();
+
+    const email = ref('');
+    const password = ref('');
+
+    const handleSubmit = async () =>{
+      const res = await login(email.value, password.value);
+      if(!error.value){
+        console.log('user logged in', res);
+      }
+
+    }
+
+    return {email, password, handleSubmit, error, isPending}
+  }
+}
+</script>
+
+<style>
+
+</style>
