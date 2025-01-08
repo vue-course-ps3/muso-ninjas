6d9d7a99b55818739d1db2cb1bd17c05589c2d5c
maxim-peeters <maximpeeters38@gmail.com>
2025-01-09 00:51:37 +0100

    lesson 167

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script>
+import getUser from '@/composables/getUser';
+import getCollection from '@/composables/getCollection';
 export default {
-
+  setup(){
+    const {user} = getUser();
+    const { documents: playlists } = getCollection('playlists', ['userId', '==', user.value.uid])
+    console.log(playlists)
+    return { playlists }
+  }
 }
 </script>
 
